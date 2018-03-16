de38996f1184e48f3a9bbd6de5a5a6c64b0baa67
Robert Willemelis <willemelis@strato.de>
2018-03-16 17:00:25 +0100

    feat(tests): e2e

---
+++ conf/single.conf.js
@@ -0,0 +1,14 @@
+exports.config = {
+  'specs': [ '../e2e/app.e2e-spec.ts' ],
+  'seleniumAddress': 'http://hub-cloud.browserstack.com/wd/hub',
+
+  'capabilities': {
+    'browserstack.user': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+    'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+    'build': 'protractor-browserstack',
+    'name': 'single_test',
+    'browserName': 'chrome',
+    'resolution': '1024x768',
+    'browserstack.debug': 'true'
+  }
+};
